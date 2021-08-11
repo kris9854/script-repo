1c91fb9d87d92c5128ff2e4a0be00662045299a2
Kristian <kris9854@gmail.com>
2021-08-11 22:29:07 +0200

    updated the install-env script

--- bash_scripts/install-env.sh
+++ bash_scripts/install-env.sh
@@ -2,11 +2,10 @@
 echo "Upgrading and updating machine"
 sudo apt update -y
 apt-get upgrade -y
-
 echo "installing ansible for Ubuntu"
 sudo apt install software-properties-common -y 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-
-echo "installing paramiko"
-sudo pip install paramiko
+echo "installing pip and paramiko"
+sudo apt-get -y install python3-pip
+sudo pip3 install paramiko
