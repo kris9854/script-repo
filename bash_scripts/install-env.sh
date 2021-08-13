78b245de56a68cc095de2f265c0c2044e7b5e4ad
Kristian <43323270+kris9854@users.noreply.github.com>
2021-08-13 11:41:06 +0200

    Update install-env.sh

--- bash_scripts/install-env.sh
+++ bash_scripts/install-env.sh
@@ -25,3 +25,16 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo add-apt-repository universe
   # Install PowerShell
   sudo apt-get install -y powershell
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+fi
